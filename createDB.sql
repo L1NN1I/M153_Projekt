6772abccdc4c74d40a89934b7936edc12e936788
L1NN1I <linus.oberli@gmail.com>
2023-05-05 16:00:33 +0200

    Updated SQL Script Linus (20min)

--- createDB.sql
+++ createDB.sql
@@ -1,50 +1,57 @@
-IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'Autoverleih_db')
-	BEGIN
-		CREATE DATABASE [Autoverleih_db]
-	END
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'AutoverleihDB')
+    BEGIN
+        CREATE DATABASE [AutoverleihDB]
+    END
 GO
 
-USE [Autoverleih_db]
+USE [AutoverleihDB]
 GO
 
-DROP TABLE IF EXISTS dbo.Zahlungen;
-DROP TABLE IF EXISTS dbo.Vermietungen;
-DROP TABLE IF EXISTS dbo.Kunden;
-DROP TABLE IF EXISTS dbo.Fahrzeuge;
+DROP TABLE IF EXISTS dbo.Vermietung;
+DROP TABLE IF EXISTS dbo.FahrzeugKategorieZuordnung;
+
+DROP TABLE IF EXISTS dbo.Kunde;
+DROP TABLE IF EXISTS dbo.Fahrzeug;
+DROP TABLE IF EXISTS dbo.FahrzeugKategorie;
 GO
 
-CREATE TABLE dbo.Fahrzeuge (
+CREATE TABLE dbo.Fahrzeug (
     FahrzeugID INT PRIMARY KEY IDENTITY,
     Marke NVARCHAR(50) NOT NULL,
     Modell NVARCHAR(50) NOT NULL,
     Baujahr INT NOT NULL,
-    Preis_pro_Tag DECIMAL(10, 2) NOT NULL
+    PreisProTag DECIMAL(10, 2) NOT NULL
 )
 GO
 
-CREATE TABLE dbo.Kunden (
-    KundenID INT PRIMARY KEY IDENTITY,
+CREATE TABLE dbo.Kunde (
+    KundeID INT PRIMARY KEY IDENTITY,
     Vorname NVARCHAR(50) NOT NULL,
     Nachname NVARCHAR(50) NOT NULL,
     Telefon NVARCHAR(20) NOT NULL,
-    [E-Mail] NVARCHAR(100) NOT NULL
+    EMail NVARCHAR(100) NOT NULL
 )
 GO
 
-CREATE TABLE dbo.Vermietungen (
-    VermietungsID INT PRIMARY KEY IDENTITY,
-    KundenID INT FOREIGN KEY REFERENCES Kunden(KundenID),
-    FahrzeugID INT FOREIGN KEY REFERENCES Fahrzeuge(FahrzeugID),
+CREATE TABLE dbo.Vermietung (
+    VermietungID INT PRIMARY KEY IDENTITY,
+    fk_KundeID INT FOREIGN KEY REFERENCES Kunde(KundeID),
+    fk_FahrzeugID INT FOREIGN KEY REFERENCES Fahrzeug(FahrzeugID),
     Mietbeginn DATE NOT NULL,
     Mietende DATE NOT NULL
 )
 GO
 
-CREATE TABLE dbo.Zahlungen (
-    ZahlungsID INT PRIMARY KEY IDENTITY,
-    VermietungsID INT FOREIGN KEY REFERENCES Vermietungen(VermietungsID),
-    Betrag DECIMAL(10, 2) NOT NULL,
-    Zahlungsdatum DATE NOT NULL,
-    Zahlungsmethode NVARCHAR(50) NOT NULL
+CREATE TABLE dbo.FahrzeugKategorie (
+    KategorieID INT PRIMARY KEY IDENTITY,
+    KategorieName NVARCHAR(50) NOT NULL,
+    KategorieBeschreibung NVARCHAR(255) NULL
+)
+GO
+
+CREATE TABLE dbo.FahrzeugKategorieZuordnung (
+    fk_FahrzeugID INT FOREIGN KEY REFERENCES Fahrzeug(FahrzeugID),
+    fk_KategorieID INT FOREIGN KEY REFERENCES FahrzeugKategorie(KategorieID),
+    PRIMARY KEY (fk_FahrzeugID, fk_KategorieID)
 )
 GO
